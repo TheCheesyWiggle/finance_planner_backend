64e51651199b997e4d8b5ded7205ce9e66fbf00a
Finn <fjlvanmontfort@gmail.com>
2022-12-03 18:06:42 +0000

    Finished objects

--- src/Objects/Receipt.java
+++ src/Objects/Receipt.java
@@ -1,4 +1,57 @@
 package Objects;
 
 public class Receipt {
+    private int id;
+    private String company;
+    private int amount;
+    private String type;
+    private String notes;
+
+    public Receipt(int id, String company, int amount, String type, String notes) {
+        this.id = id;
+        this.company = company;
+        this.amount = amount;
+        this.type = type;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
--- src/Objects/Subscription.java
+++ src/Objects/Subscription.java
@@ -1,4 +1,66 @@
 package Objects;
 
 public class Subscription {
+    private int id;
+    private String company_name;
+    private int amount;
+    private int occurrence;
+    private String type;
+    private String notes;
+
+    public Subscription(int id, String company_name, int amount, int occurence, String notes) {
+        this.id = id;
+        this.company_name = company_name;
+        this.amount = amount;
+        this.occurrence = occurence;
+        this.type = type;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getOccurrence() {
+        return occurrence;
+    }
+
+    public void setOccurrence(int occurrence) {
+        this.occurrence = occurrence;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+        public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
